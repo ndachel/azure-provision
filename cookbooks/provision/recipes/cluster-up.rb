653c790f35e64211628e0ea7b930da31cc0461ef
Roseville DCIA Team <rosevilledciateam@Rosevilles-MacBook-Pro.local>
2015-10-16 14:48:13 -0700

    I'm pretty much amazing

--- cookbooks/provision/recipes/cluster-up.rb
+++ cookbooks/provision/recipes/cluster-up.rb
@@ -10,27 +10,20 @@ with_driver 'azure'
 
 machine_options = {
     :bootstrap_options => {
-      :vm_user => 'localadmin', #required if Windows
-      :cloud_service_name => 'chefprovisioning', #required
-      :storage_account_name => 'chefprovisioning', #required
-      :vm_size => 'Standard_D1', #optional
-      :location => 'West US', #optional
-      :tcp_endpoints => '3389:3389', #optional
-      :winrm_transport => { #optional
-        'https' => { #required (valid values: 'http', 'https')
-          :disable_sspi => false, #optional, (default: false)
-          :basic_auth_only => false, #optional, (default: false)
-          :no_ssl_peer_verification => true #optional, (default: false)
-        }
-      }
+      :cloud_service_name => 'mbfrahryTest', #required
+      :storage_account_name => 'mbfrahrytest', #required
+      :vm_size => "Standard_D1", #required
+      :location => 'Central US', #required
+      :tcp_endpoints => '80:80' #optional
     },
-    :password => 'P2ssw0rd', #required
-    :image_id => 'a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-R2-201506.01-en.us-127GB.vhd' #required
+    :image_id => 'b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-14_04_2-LTS-amd64-server-20150706-en-us-30GB', #required
+    # Until SSH keys are supported (soon)
+    :password => "Test12" #required
 }
 
 machine_batch do
   1.upto(cloud_count) do |i|
-    machine "cluster-cloud-#{i}" do
+    machine "mbfrahry-#{i}" do
       machine_options machine_options
       run_list my_run_list
     end
